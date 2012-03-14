142ccfafdeacc19404ce6d67f8b7b138cacd3555
Tomo Ferega <tomo@element.hr>
2012-03-14 17:40:36 +0100

    Added timing for operations for S3.

--- src/main/scala/hr/element/doit/s3/S3.scala
+++ src/main/scala/hr/element/doit/s3/S3.scala
@@ -2,6 +2,7 @@ package hr.element.doit
 package s3
 
 import serio.{ Decode, Encode }
+import statistics.Timer
 
 import com.amazonaws.{ AmazonClientException, AmazonServiceException }
 import com.amazonaws.auth.BasicAWSCredentials
@@ -30,50 +31,67 @@ case class S3Bucket(
     secret:      Option[Array[Byte]] = None)
 
 
+
+case class S3Result[T](totalTime: Long, actionTime: Option[Long], result: Either[Exception, T])
 object S3 {
+  def ErrorHanlder[T]: PartialFunction[Throwable, (Option[Long], Either[Exception, T])] = {
+    case e: AmazonServiceException => (None, Left(new Exception("An error occured in Amazon S3 while processing the request.", e)))
+    case e: AmazonClientException  => (None, Left(new Exception("An error occured in the client while making the request or handling the response.", e)))
+    case e: Exception              => (None, Left(new Exception("An unexpected error occured.", e)))
+  }
+
+
   // Limits number of concurrent operations to 1000.
   implicit val strat = Strat.cachedTo (1000)
 
   def store[A]
       (key: String, data: A)
       (implicit bucket: S3Bucket, ev: Encode[A]):
-      Promise[Either[Exception, Unit]] =
+      Promise[S3Result[Unit]] =
     promise {
-      try {
-        val encodedData = Encode.encode[A](data)
-        val client = S3Gate.clients((bucket.credentials, bucket.secret))
+      val timedResult = Timer {
+        try {
+          val encodedData = Encode.encode[A](data)
+          val client = S3Gate.clients((bucket.credentials, bucket.secret))
 
-        val meta = new ObjectMetadata
-        meta setContentLength encodedData.length
+          val meta = new ObjectMetadata
+          meta setContentLength encodedData.length
 
-        client.putObject(
-          bucket.name, key, new ByteArrayInputStream(encodedData), meta)
+          val dataStream = new ByteArrayInputStream(encodedData)
+          val timedPut = Timer {
+            client.putObject(bucket.name, key, dataStream, meta)
+          }
 
-        Right()
-      } catch {
-        case e: AmazonClientException  => Left(new Exception("An error occured in the client while making the request or handling the response.", e))
-        case e: AmazonServiceException => Left(new Exception("An error occured in Amazon S3 while processing the request.", e))
-        case e: Exception              => Left(new Exception("An unexpected error occured.", e))
+          (Some(timedPut.time), Right())
+        } catch
+          ErrorHanlder
       }
+
+      val Timer(totalTime, (actionTime, result)) = timedResult
+      S3Result(totalTime, actionTime, result)
     }
 
 
   def load[A](key: String)
       (implicit bucket: S3Bucket, ev: Decode[A]):
-      Promise[Either[Exception, A]] =
+      Promise[S3Result[A]] =
     promise {
-      try {
-        val client   = S3Gate.clients((bucket.credentials, bucket.secret))
-        val response = client.getObject(bucket.name, key)
-        val result   = Decode.decode[A](toByteArray(response.getObjectContent))
+      val timedResult = Timer {
+        try {
+          val client   = S3Gate.clients((bucket.credentials, bucket.secret))
+          val response = client.getObject(bucket.name, key)
+          val timedGet = Timer {
+            Decode.decode[A](toByteArray(response.getObjectContent))
+          }
 
-        Right(result)
-      } catch {
-        case e: AmazonClientException  => Left(new Exception("An error occured in the client while making the request or handling the response.", e))
-        case e: AmazonServiceException => Left(new Exception("An error occured in Amazon S3 while processing the request.", e))
-        case e: Exception              => Left(new Exception("An unexpected error occured.", e))
+          (Some(timedGet.time), Right(timedGet.result))
+        } catch
+          ErrorHanlder
       }
-  }
+
+      val Timer(totalTime, (actionTime, result)) = timedResult
+      S3Result(totalTime, actionTime, result)
+    }
 }
 
 
